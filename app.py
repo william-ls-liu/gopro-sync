9d171e7b0e7f3fe0e1f5c36b46578ab2a1040868
William Liu <will123liu@gmail.com>
2024-12-10 10:55:19 -0800

    chore: update release version

--- app.py
+++ app.py
@@ -617,7 +617,7 @@ logging.basicConfig(
     format="%(asctime)s:%(levelname)s:%(message)s",
     level=logging.INFO,
 )
-RELEASE_VERSION = "v0.2.2"
+RELEASE_VERSION = "v0.2.3"
 logging.info(f"Using gopro-sync version: {RELEASE_VERSION}.")
 
 console = Console()
